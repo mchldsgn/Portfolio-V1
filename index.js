3b43d81fd5cb1f635d068867cbe99a596fcdb594
Gaelle M <gsmichel0@gmail.com>
2023-06-13 22:03:37 -0400

    updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const closeNavButton = document.querySelector('#nav-content .close-btn');
 const navLinks = document.querySelectorAll('#nav-content nav ul li a');
 const scrollButton = document.querySelector(".scroll-top");
 
-// scroll TOP Button Events
+// Scroll TOP Button Events
 
 if(scrollButton){
   window.addEventListener('scroll', ()=> {
@@ -34,3 +34,48 @@ navLinks.forEach( link => {
     document.body.style.overflow="initial";
   })
 })
+
+// Project Filter
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
